6e0a8e0a02df23fe958dc9a3497bcb4446a23481
Kory Smith <kory.smith@kroger.com>
2023-12-05 14:55:40 -0500

    adds start of redux build-your-own

---
+++ redux/index.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from 'react-dom/client';
+
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+
+function App() {
+	return (<h1>Hey</h1>)
+}
+root.render(<App />);
