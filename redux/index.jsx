a68f4b11eb7fd92f4b70e81c6b951b8eb5e8fb91
Kory Smith <kory.smith@kroger.com>
2023-12-05 15:27:51 -0500

    redux: adds helpful comment showing source

--- redux/index.jsx
+++ redux/index.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
 
+// Following https://zapier.com/engineering/how-to-build-redux/
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
 
